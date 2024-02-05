2759d25149fd9f286e6e38a8cbd04a8cb01c73d1
joaco-basile <joacobasile@gmail.com>
2024-02-05 20:09:35 -0300

    cambios para poder hacer build

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -5,28 +5,36 @@ export default function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const menuItems = [
-    "¿Quien somos?",
-    "Productos",
-    "Servicio de entrega",
-    "Puntos de venta"
+    {
+      title: "¿Quien somos?",
+      url: "#quiensomos"
+    },
+    {
+      title: "Productos",
+      url:"#productos"
+    },
+    {
+      title: "Puntos de venta",
+      url:"#puntosdeventa"
+    }
   ];
 
   return (
-    <Navbar className="dark h-[20vh] bg-black" onMenuOpenChange={setIsMenuOpen}>
+    <Navbar className="dark bg-black" height="7rem" maxWidth="xl" onMenuOpenChange={setIsMenuOpen} isMenuOpen={isMenuOpen}>
       <NavbarContent>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden text-white"
+          className="md:hidden text-white"
         />
         <NavbarBrand>
           <img src="http://www.ladrillerabasile.com.ar/images/logo.png" className="h-10 lg:h-16"/>
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarContent className="hidden md:flex gap-4" justify="center">
         <NavbarItem>
           <Link href="#quienesSomos" className="text-foreground">
-            ¿Quien somos?
+            ¿Quienes somos?
           </Link>
         </NavbarItem>
         <NavbarItem>
@@ -35,22 +43,32 @@ export default function NavBar() {
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link href="#" className="text-foreground">
+          <Link href="#puntosdeventa" className="text-foreground">
             Puntos de venta
           </Link>
         </NavbarItem>
+        <NavbarItem>
+          <a href="http://www.instagram.com/ladrillerabasile/" target="_blank">
+            <img src="/instagramIcon.png" alt="icon" width={20} />
+          </a>
+        </NavbarItem>
+        <NavbarItem>
+          <a href="http://www.facebook.com/ladrillerabasile" target="_blank">
+            <img src="/facebookIcon.png" alt="icon" width={18} />
+          </a>
+        </NavbarItem>
       </NavbarContent>
-
       <NavbarMenu>
         {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
+          <NavbarMenuItem key={`${item.title}-${index}`}>
             <Link
               color="primary"
               className="w-full"
-              href="#"
+              href={item.url}
               size="lg"
+              onClick={()=> {setIsMenuOpen(false)}}
             >
-              {item}
+              {item.title}
             </Link>
           </NavbarMenuItem>
         ))}
