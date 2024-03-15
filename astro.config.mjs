5fafdd90d25edb3930dca3108c46c11cb840c6ed
joaco-basile <joacobasile@gmail.com>
2024-03-15 19:43:32 -0300

    cambiando la url de la config de astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://joaco-basile.github.io'
+  site: 'https://joaco-basile.github.io/ladrillera-basile/',
 });
